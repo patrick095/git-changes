b630d84e5dd5329e7c0d21495b7e8424f2d2cf27
Patrick Douglas Pinho Chaves <pdchaves@stefanini.com>
2024-03-27 11:22:27 -0300

    [feat] adicionado download json para entregador de ofs

--- src/controllers/git.controller.ts
+++ src/controllers/git.controller.ts
@@ -7,6 +7,9 @@ import { TaskRepository } from "../repositories/task.repository";
 import { BaseService } from "../service/base.service";
 import { Git } from "../git";
 import { PlataformaService } from "../service/plataforma.service";
+import { readFile } from "fs";
+import { join } from "path";
+import { FileGroupByCategory } from "../interfaces/file.interface";
 
 export class GitController implements ControllerInterface {
     private _router: Router;
@@ -114,6 +117,41 @@ export class GitController implements ControllerInterface {
             });
     }
 
+    private async getJsonDownload(req: Request, res: Response) {
+        let valorTotal = 0;
+        readFile(join(__dirname + '/../repositories/git-all-category.json'), { encoding: 'utf-8' } , async (err, data) => {
+            if (err) {
+                res.status(500).json({ status: 500, message: 'Erro ao gerar json para download' });
+            }
+            const jsonData = JSON.parse(data) as Array<FileGroupByCategory>;
+            const entregas = jsonData.map((category) => {
+                valorTotal += category.pontuacao as number;
+                return {
+                    itemGuia: category.categoria,
+                    complexidade: 'Padrão',
+                    itens: category.arquivos.map((file) => {
+                        const [ caminho, descricao ] = file.split(';');
+                        return {
+                            caminho,
+                            descricao
+                        }
+                    })
+                }
+            })
+
+            const user = await this.service.get<{ username: string }>('/user');
+
+            const finalData = {
+                chave: user.username,
+                numeroOF: 0,
+                numeroOrdemContratacao: 0,
+                valorOF: valorTotal,
+                entregas
+            }
+            res.status(200).json(finalData);
+        })
+    }
+
     private _registerRouters(): void {
         this._router.post("/git-info", this.saveGitInfo.bind(this));
         this._router.get("/git-info", this.getGitInfo.bind(this));
@@ -123,5 +161,6 @@ export class GitController implements ControllerInterface {
         this._router.get('/git-commits', this.getGitCommitsNumber.bind(this));
         this._router.post('/git-update', this.getGitAllData.bind(this));
         this._router.get("/git-connection", this.getUserId.bind(this));
+        this._router.get("/git-json-download", this.getJsonDownload.bind(this));
     }
 }
